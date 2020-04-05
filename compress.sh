22e9a16640894ee071bd8f2e6982cc39c4892b5e
Kiarash Banihashem <k.banihashem73@gmail.com>
2020-04-05 15:43:22 +0430

    experimenting with compress

---
+++ compress.sh
@@ -0,0 +1,4 @@
+rm project1-96109963.zip
+cp {bigram,document,evaluate,helper,index_construction}.py project1-96109963/
+cp MIRProjectPhase1.ipynb project1-96109963/
+zip -r project1-96109963.zip project1-96109963/
