fb3c116fb52410bbe4779d65390b72f27c47193c
Kiarash Banihashem <k.banihashem73@gmail.com>
2020-04-05 09:47:22 +0430

    worked on general stuff for bigram

--- bigram.py
+++ bigram.py
@@ -3,13 +3,19 @@ class Bigram:
     def __init__(self):
         #bigram -> {word1, word2, ... wordn}
         self.bigram_index = {}
+        self.word_set = set()
 
     def add_word(self, word):
+        self.word_set.add(word)
         bigrams = Bigram.shatter(word)
         for bigram in bigrams:
             self.bigram_index.setdefault(bigram, []).add(word)
 
     def remove_word(self, word):
+
+        if word in self.word_set:
+            self.word_set.remove(word)
+
         bigrams = Bigram.shatter(word)
         for bigram in bigrams:
             bigram_set = self.bigram_index[bigram]
@@ -21,3 +27,16 @@ class Bigram:
         n = len(word)
         return [word[i:i + 2] for i in range(n - 1)]
 
+    @staticmethod
+    def edit_distance(word1, word2):
+        pass
+
+    def extract_top_bigram(self, word):
+        pass
+
+    def fix_word(self, word):
+        if w in self.word_set:
+            return w
+
+        candidates = self.extract_top_bigram(word)
+        return max(candidates, key = lambda w: edit_distance(word, w), reverse=True)
